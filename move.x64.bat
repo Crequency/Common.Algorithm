d3815eaa32dcddbc3d84aac276d2117662ca6e27
Dynesshely <2369913161@qq.com>
2022-03-22 17:30:01 +0800

    📝 Chore: Copy files script

---
+++ move.x64.bat
@@ -0,0 +1,9 @@
+
+copy .\out\build\x64-debug\Hash\Hash.dll .\build\Core\
+
+
+copy .\Algorithm.Interop\bin\Debug\net6.0\Algorithm.Interop.dll .\build\
+
+
+
+pause
---
+++ move.x64.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+echo "Start building up project, copying files ... "
+
+buildup="./build/"
+cmake="./out/build/x64-debug/"
+com_cspro="bin/Debug/net6.0"
+binFiles="./build/"
+coreFiles="./build/Core"
+
+echo "Copying CS Dll Project:"
+for i in "Algorithm.Interop"; do
+    echo "    $i"
+    echo "        cp ./$i/$com_cspro/$i.dll $binFiles"
+    cp ./$i/$com_cspro/$i.dll $binFiles
+done
+
+echo "Copying CMake Algorithm Project:"
+for i in "Hash"; do
+    echo "    $i"
+    echo "        cp $cmake/$i/$i.dll $coreFiles"
+    cp $cmake/$i/$i.dll $coreFiles
+done
+
+sleep 3
