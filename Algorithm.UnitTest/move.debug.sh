ac06f008547d355eb95590399472b1c2ace3e515
Dynesshely <2369913161@qq.com>
2022-03-26 20:44:03 +0800

    📝 Chore: Add rm

--- Algorithm.UnitTest/move.debug.sh
+++ Algorithm.UnitTest/move.debug.sh
@@ -3,6 +3,7 @@ basePath=../build/Core
 tardi=./bin/Debug/Core
 mkdir $tardi
 for i in "Hash" "Math"; do
+    rm -f $tardi/$i.dll
     cp $basePath/$i.dll $tardi/
     echo "cp $basePath/$i.dll $tardi/$i.dll"
 done
