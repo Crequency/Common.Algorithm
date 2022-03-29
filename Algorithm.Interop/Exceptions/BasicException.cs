aa9572f44958285306bcbebe4d441de877bd33b8
Dynesshely <2369913161@qq.com>
2022-03-29 17:12:55 +0800

    💾 Feat: Exceptions

---
+++ Algorithm.Interop/Exceptions/BasicException.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithm.Interop.Exceptions
+{
+    public class BasicException : Exception
+    {
+        /// <summary>
+        /// 异常描述
+        /// </summary>
+        public string? Description { get; protected set; }
+
+        /// <summary>
+        /// 是否可以被解决而不影响运行
+        /// </summary>
+        public bool CanHandle { get; set; }
+
+        /// <summary>
+        /// 异常严重性
+        /// </summary>
+        public ExceptionLevel Level { get; set; }
+
+        /// <summary>
+        /// 构造函数
+        /// </summary>
+        public BasicException()
+        {
+
+        }
+
+        /// <summary>
+        /// 构造函数
+        /// </summary>
+        /// <param name="message">消息</param>
+        public BasicException(string message) : base(message)
+        {
+
+        }
+
+        /// <summary>
+        /// 构造函数
+        /// </summary>
+        /// <param name="message">消息</param>
+        /// <param name="descr">描述</param>
+        public BasicException(string message, string descr) : base(message)
+        {
+            Description = descr;
+        }
+
+        /// <summary>
+        /// 异常级别
+        /// </summary>
+        public enum ExceptionLevel
+        {
+            Info = 0, Warning = 1, Error = 2, Fatal = 3, Disaster = 4
+        }
+    }
+}
