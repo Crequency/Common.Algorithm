f42e1c3d1301d28355038401563ecbb964808b64
Dynesshely <2369913161@qq.com>
2022-03-31 13:49:31 +0800

    💾 Feat: Include <cstdarg>

--- Base/base.h
+++ Base/base.h
@@ -3,12 +3,14 @@
 //#include <stdlib.h>
 #include <iostream>
 #include <algorithm>
+#include <cstdarg>
 
 #define EXTERN_API __declspec(dllexport)
 
 #define uchar unsigned char
 #define ull unsigned long long
 #define ll long long
+#define uint unsigned int
 
 #define st_ca static_cast
 #define st_ca_int static_cast<int>
