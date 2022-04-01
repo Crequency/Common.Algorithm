982d5f481ddc4120d0ffd3b060d5a885531c6820
Dynesshely <2369913161@qq.com>
2022-04-01 20:43:52 +0800

    💾 Feat: Func -> maxin, gobit <- added!

--- Math/Math.h
+++ Math/Math.h
@@ -1,9 +1,15 @@
 #include "../Base/base.h"
 
 namespace calg{
-    extern "C" EXTERN_API inline ll max(ll x, ...);
-    extern "C" EXTERN_API inline ll min(ll x, ...);
-    extern "C" EXTERN_API inline ll abs(ll x);
-    extern "C" EXTERN_API inline ll pow(ll x, ll t);
-    extern "C" EXTERN_API inline ll gcd(ll a, ll b);
+    const long double pi = 3.14159265358979323846;
+    const long double e = 2.71828182845904523536;
+
+    EXTERN_ALL inline i64 max(i64 x, ...);
+    EXTERN_ALL inline i64 min(i64 x, ...);
+    EXTERN_ALL inline i64 mid(i64 a, i64 b, i64 c);
+    EXTERN_ALL inline void maxin(i64 *max, i64 *min, i64 x, ...);
+    EXTERN_ALL inline i64 abs(i64 x);
+    EXTERN_ALL inline i64 pow(i64 x, i64 t);
+    EXTERN_ALL inline i64 gcd(i64 a, i64 b);
+    EXTERN_ALL inline i64 gobit(i64 x, i32 len);
 }
