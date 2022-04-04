8369e5cd5f15b936262d870b79aa427d11f80ae9
Dynesshely <2369913161@qq.com>
2022-04-04 17:36:24 +0800

    💾 Feat: New hash solved UB. Close #1.
    New issue: 雪崩效应简直是没有

--- Hash/Hash.h
+++ Hash/Hash.h
@@ -11,6 +11,7 @@ namespace calg{
     inline i32 mix_3(i32 a, i32 b, i32 c);
     inline i32 mix_5(i32 a, i32 b, i32 c, i32 d, i32 e);
     inline void exp_1(i32 x, i32 *a, i32 *b, i32 *c);
+    inline void exp_1(i32 x, i32 *a, i32 *b, i32 *c, i32 *d);
     inline long double spring_func(long double x);
     EXTERN_ALL void hash_str(uchar *src, uchar *rst, int length);
     EXTERN_ALL void hash_compress_128_str(uchar *src, uchar *rst);
@@ -19,6 +20,6 @@ namespace calg{
     EXTERN_ALL void hash_compress_16_str(uchar *src, uchar *rst);
     EXTERN_ALL void hash_compress_8_str(uchar *src, uchar *rst);
     EXTERN_ALL void hash_compress_4_str(uchar *src, uchar *rst);
-    EXTERN_ALL int hash_file(uchar * fileName, int type);
+    EXTERN_ALL int hash_file(uchar *fileName, int type);
 }
 
