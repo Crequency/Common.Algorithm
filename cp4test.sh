67c50ce4be92c6244f9fa0402f3799c418db8a7e
Dynesshely <catrol@qq.com>
2023-03-21 19:50:17 +0800

    📝 Chore: Shell scripts for test cpp libs in interop unit tests.

---
+++ cp4test.sh
@@ -0,0 +1,9 @@
+﻿
+cppOut=Common.Algorithm/out/build/x64-Debug/
+desDir=Common.Algorithm.Interop.Test/bin/Debug/net6.0/Core
+
+rm -r $desDir
+mkdir $desDir
+
+cp ${cppOut}Hash/Hash.dll $desDir
+cp ${cppOut}Math/Math.dll ${desDir}/
