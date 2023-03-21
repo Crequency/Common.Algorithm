463c218b8700bf2f1a6b9077582468fada77bc2f
Dynesshely <catrol@qq.com>
2023-03-21 18:19:23 +0800

    🎇 Style: 使用文件命名空间

--- Common.Algorithm.Interop/Environment.cs
+++ Common.Algorithm.Interop/Environment.cs
@@ -2,131 +2,130 @@
 
 #pragma warning disable SYSLIB0014 // 类型或成员已过时
 
-namespace Common.Algorithm.Interop
+namespace Common.Algorithm.Interop;
+
+public static class Environment
 {
-    public static class Environment
+
+    /// <summary>
+    /// 核心文件路径
+    /// </summary>
+    public const string DllPath = "./Core/";
+
+    /// <summary>
+    /// 云端 DLL 存储路径
+    /// </summary>
+    private static string _cloudUrl = "https://source.catrol.cn/lib/Algorithm/Core/";
+
+    /// <summary>
+    /// 库版本
+    /// </summary>
+    private static string _version = "v1.0";
+
+    /// <summary>
+    /// 核心文件文件名
+    /// </summary>
+    private static readonly List<string> CoreFiles = new()
     {
+        "Math.dll",
+        "Hash.dll"
+    };
 
-        /// <summary>
-        /// 核心文件路径
-        /// </summary>
-        public const string DllPath = "./Core/";
+    /// <summary>
+    /// 更新云端存储链接
+    /// </summary>
+    /// <param name="url"></param>
+    public static void UpdateCloudStorageUrl(string url) => _cloudUrl = url;
 
-        /// <summary>
-        /// 云端 DLL 存储路径
-        /// </summary>
-        private static string _cloudUrl = "https://source.catrol.cn/lib/Algorithm/Core/";
+    /// <summary>
+    /// 更新云端存储版本
+    /// </summary>
+    /// <param name="ver"></param>
+    public static void UpdateCloudStorageVersion(string ver) => _version = ver;
 
-        /// <summary>
-        /// 库版本
-        /// </summary>
-        private static string _version = "v1.0";
-
-        /// <summary>
-        /// 核心文件文件名
-        /// </summary>
-        private static readonly List<string> CoreFiles = new()
+    /// <summary>
+    /// 检查环境是否就绪, 核心文件是否存在
+    /// </summary>
+    /// <returns>环境是否就绪</returns>
+    public static bool CheckEnvironment()
+    {
+        foreach (string fn in CoreFiles)
         {
-            "Math.dll",
-            "Hash.dll"
-        };
-
-        /// <summary>
-        /// 更新云端存储链接
-        /// </summary>
-        /// <param name="url"></param>
-        public static void UpdateCloudStorageUrl(string url) => _cloudUrl = url;
-
-        /// <summary>
-        /// 更新云端存储版本
-        /// </summary>
-        /// <param name="ver"></param>
-        public static void UpdateCloudStorageVersion(string ver) => _version = ver;
-
-        /// <summary>
-        /// 检查环境是否就绪, 核心文件是否存在
-        /// </summary>
-        /// <returns>环境是否就绪</returns>
-        public static bool CheckEnvironment()
-        {
-            foreach (string fn in CoreFiles)
-            {
-                if (!File.Exists(Path.GetFullPath($"{DllPath}{fn}")))
-                    return false;
-            }
-            return true;
+            if (!File.Exists(Path.GetFullPath($"{DllPath}{fn}")))
+                return false;
         }
+        return true;
+    }
 
-        /// <summary>
-        /// 安装环境
-        /// <paramref name="im">安装方式</paramref>
-        /// </summary>
-        public static void InstallEnvironment(InstallMethod im = InstallMethod.WebClient)
+    /// <summary>
+    /// 安装环境
+    /// <paramref name="im">安装方式</paramref>
+    /// </summary>
+    public static void InstallEnvironment(InstallMethod im = InstallMethod.WebClient)
+    {
+        if (!Directory.Exists(DllPath))
+            Directory.CreateDirectory(DllPath);
+        switch (im)
         {
-            if (!Directory.Exists(DllPath))
-                Directory.CreateDirectory(DllPath);
-            switch (im)
-            {
-                case InstallMethod.WebClient:
-                    WebClient wc = new();
-                    foreach (string fn in CoreFiles)
+            case InstallMethod.WebClient:
+                WebClient wc = new();
+                foreach (string fn in CoreFiles)
+                {
+                    if (!File.Exists($"{DllPath}{fn}"))
                     {
-                        if (!File.Exists($"{DllPath}{fn}"))
-                        {
-                            wc.DownloadFile(
-                                new Uri($"{_cloudUrl}{_version}/{fn}", UriKind.Absolute),
-                                Path.GetFullPath($"{DllPath}{fn}"));
-                        }
+                        wc.DownloadFile(
+                            new Uri($"{_cloudUrl}{_version}/{fn}", UriKind.Absolute),
+                            Path.GetFullPath($"{DllPath}{fn}"));
                     }
-                    wc.Dispose();
-                    break;
-            }
+                }
+                wc.Dispose();
+                break;
         }
+    }
 
-        /// <summary>
-        /// 异步安装环境
-        /// <paramref name="im">异步安装方式</paramref>
-        /// </summary>
-        public static async Task InstallEnvironmentAsync(InstallMethodAsync im = InstallMethodAsync.WebClientAsync)
+    /// <summary>
+    /// 异步安装环境
+    /// <paramref name="im">异步安装方式</paramref>
+    /// </summary>
+    public static async Task InstallEnvironmentAsync(InstallMethodAsync im = InstallMethodAsync.WebClientAsync)
+    {
+        if (!Directory.Exists(DllPath))
+            Directory.CreateDirectory(DllPath);
+        switch (im)
         {
-            if (!Directory.Exists(DllPath))
-                Directory.CreateDirectory(DllPath);
-            switch (im)
-            {
-                case InstallMethodAsync.WebClientAsync:
-                    var wc = new WebClient();
-                    foreach (string fn in CoreFiles)
+            case InstallMethodAsync.WebClientAsync:
+                var wc = new WebClient();
+                foreach (string fn in CoreFiles)
+                {
+                    if (!File.Exists($"{DllPath}{fn}"))
                     {
-                        if (!File.Exists($"{DllPath}{fn}"))
-                        {
-                            await wc.DownloadFileTaskAsync(
-                                new Uri($"{_cloudUrl}{_version}/{fn}", UriKind.Absolute),
-                                Path.GetFullPath($"{DllPath}{fn}"));
-                        }
+                        await wc.DownloadFileTaskAsync(
+                            new Uri($"{_cloudUrl}{_version}/{fn}", UriKind.Absolute),
+                            Path.GetFullPath($"{DllPath}{fn}"));
                     }
-                    wc?.Dispose();
-                    break;
-                case InstallMethodAsync.Http:
+                }
+                wc?.Dispose();
+                break;
+            case InstallMethodAsync.Http:
 
-                    break;
-            }
+                break;
         }
+    }
 
-        /// <summary>
-        /// 安装环境下载方式
-        /// </summary>
-        public enum InstallMethod
-        {
-            WebClient
-        }
+    /// <summary>
+    /// 安装环境下载方式
+    /// </summary>
+    public enum InstallMethod
+    {
+        WebClient
+    }
 
-        /// <summary>
-        /// 安装环境下载方式
-        /// </summary>
-        public enum InstallMethodAsync
-        {
-            WebClientAsync, Http
-        }
+    /// <summary>
+    /// 安装环境下载方式
+    /// </summary>
+    public enum InstallMethodAsync
+    {
+        WebClientAsync, Http
     }
 }
 
--- Common.Algorithm.Interop/Hash.cs
+++ Common.Algorithm.Interop/Hash.cs
@@ -2,233 +2,232 @@
 using System.Runtime.InteropServices;
 using System.Text;
 
-namespace Common.Algorithm.Interop
+namespace Common.Algorithm.Interop;
+
+/// <summary>
+/// 文件哈希类
+/// </summary>
+public static class Hash
 {
     /// <summary>
-    /// 文件哈希类
+    /// DLL 路径
     /// </summary>
-    public static class Hash
+    private const string DllPath = Environment.DllPath;
+
+    #region 扩展库函数导入 [DLLImport Hash.dll]
+
+    /// <summary>
+    /// 调用扩展库进行 Hash
+    /// </summary>
+    /// <param name="src">元数据</param>
+    /// <param name="output">输出</param>
+    /// <param name="length">长度</param>
+    /// <returns>哈希后的数据</returns>
+    [DllImport($"{DllPath}Hash.dll", EntryPoint = "hash_str")]
+    private static extern void hash_str(byte[] src, byte[] output, int length);
+
+    /// <summary>
+    /// 调用扩展库进行 Hash 压缩
+    /// </summary>
+    /// <param name="src">哈希数据</param>
+    /// <param name="output">输出</param>
+    /// <returns>哈希压缩后的数据</returns>
+    [DllImport($"{DllPath}Hash.dll", EntryPoint = "hash_compress_128_str")]
+    private static extern void hash_compress_128_str(byte[] src, byte[] output);
+
+    /// <summary>
+    /// 调用扩展库进行 Hash 压缩
+    /// </summary>
+    /// <param name="src">哈希数据</param>
+    /// <param name="output">输出</param>
+    /// <returns>哈希压缩后的数据</returns>
+    [DllImport($"{DllPath}Hash.dll", EntryPoint = "hash_compress_64_str")]
+    private static extern void hash_compress_64_str(byte[] src, byte[] output);
+
+    /// <summary>
+    /// 调用扩展库进行 Hash 压缩
+    /// </summary>
+    /// <param name="src">哈希数据</param>
+    /// <param name="output">输出</param>
+    /// <returns>哈希压缩后的数据</returns>
+    [DllImport($"{DllPath}Hash.dll", EntryPoint = "hash_compress_32_str")]
+    private static extern void hash_compress_32_str(byte[] src, byte[] output);
+
+    /// <summary>
+    /// 调用扩展库进行 Hash 压缩
+    /// </summary>
+    /// <param name="src">哈希数据</param>
+    /// <param name="output">输出</param>
+    /// <returns>哈希压缩后的数据</returns>
+    [DllImport($"{DllPath}Hash.dll", EntryPoint = "hash_compress_16_str")]
+    private static extern void hash_compress_16_str(byte[] src, byte[] output);
+
+    /// <summary>
+    /// 调用扩展库进行 Hash 压缩
+    /// </summary>
+    /// <param name="src">哈希数据</param>
+    /// <param name="output">输出</param>
+    /// <returns>哈希压缩后的数据</returns>
+    [DllImport($"{DllPath}Hash.dll", EntryPoint = "hash_compress_8_str")]
+    private static extern void hash_compress_8_str(byte[] src, byte[] output);
+
+    /// <summary>
+    /// 调用扩展库进行 Hash 压缩
+    /// </summary>
+    /// <param name="src">哈希数据</param>
+    /// <param name="output">输出</param>
+    /// <returns>哈希压缩后的数据</returns>
+    [DllImport($"{DllPath}Hash.dll", EntryPoint = "hash_compress_4_str")]
+    private static extern void hash_compress_4_str(byte[] src, byte[] output);
+
+    #endregion
+
+    #region 辅助函数
+
+    /// <summary>
+    /// 统一调用哈希压缩
+    /// </summary>
+    /// <param name="mid">源压缩中间哈希值</param>
+    /// <param name="clv">哈希压缩级别</param>
+    private static byte[] Compress(ref byte[] mid, CompressLevel clv)
     {
-        /// <summary>
-        /// DLL 路径
-        /// </summary>
-        private const string DllPath = Environment.DllPath;
-
-        #region 扩展库函数导入 [DLLImport Hash.dll]
-
-        /// <summary>
-        /// 调用扩展库进行 Hash
-        /// </summary>
-        /// <param name="src">元数据</param>
-        /// <param name="output">输出</param>
-        /// <param name="length">长度</param>
-        /// <returns>哈希后的数据</returns>
-        [DllImport($"{DllPath}Hash.dll", EntryPoint = "hash_str")]
-        private static extern void hash_str(byte[] src, byte[] output, int length);
-
-        /// <summary>
-        /// 调用扩展库进行 Hash 压缩
-        /// </summary>
-        /// <param name="src">哈希数据</param>
-        /// <param name="output">输出</param>
-        /// <returns>哈希压缩后的数据</returns>
-        [DllImport($"{DllPath}Hash.dll", EntryPoint = "hash_compress_128_str")]
-        private static extern void hash_compress_128_str(byte[] src, byte[] output);
-
-        /// <summary>
-        /// 调用扩展库进行 Hash 压缩
-        /// </summary>
-        /// <param name="src">哈希数据</param>
-        /// <param name="output">输出</param>
-        /// <returns>哈希压缩后的数据</returns>
-        [DllImport($"{DllPath}Hash.dll", EntryPoint = "hash_compress_64_str")]
-        private static extern void hash_compress_64_str(byte[] src, byte[] output);
-
-        /// <summary>
-        /// 调用扩展库进行 Hash 压缩
-        /// </summary>
-        /// <param name="src">哈希数据</param>
-        /// <param name="output">输出</param>
-        /// <returns>哈希压缩后的数据</returns>
-        [DllImport($"{DllPath}Hash.dll", EntryPoint = "hash_compress_32_str")]
-        private static extern void hash_compress_32_str(byte[] src, byte[] output);
-
-        /// <summary>
-        /// 调用扩展库进行 Hash 压缩
-        /// </summary>
-        /// <param name="src">哈希数据</param>
-        /// <param name="output">输出</param>
-        /// <returns>哈希压缩后的数据</returns>
-        [DllImport($"{DllPath}Hash.dll", EntryPoint = "hash_compress_16_str")]
-        private static extern void hash_compress_16_str(byte[] src, byte[] output);
-
-        /// <summary>
-        /// 调用扩展库进行 Hash 压缩
-        /// </summary>
-        /// <param name="src">哈希数据</param>
-        /// <param name="output">输出</param>
-        /// <returns>哈希压缩后的数据</returns>
-        [DllImport($"{DllPath}Hash.dll", EntryPoint = "hash_compress_8_str")]
-        private static extern void hash_compress_8_str(byte[] src, byte[] output);
-
-        /// <summary>
-        /// 调用扩展库进行 Hash 压缩
-        /// </summary>
-        /// <param name="src">哈希数据</param>
-        /// <param name="output">输出</param>
-        /// <returns>哈希压缩后的数据</returns>
-        [DllImport($"{DllPath}Hash.dll", EntryPoint = "hash_compress_4_str")]
-        private static extern void hash_compress_4_str(byte[] src, byte[] output);
-
-        #endregion
-
-        #region 辅助函数
-
-        /// <summary>
-        /// 统一调用哈希压缩
-        /// </summary>
-        /// <param name="mid">源压缩中间哈希值</param>
-        /// <param name="clv">哈希压缩级别</param>
-        private static byte[] Compress(ref byte[] mid, CompressLevel clv)
+        switch (clv)
         {
-            switch (clv)
-            {
-                case CompressLevel.x128:
-                    var ans128 = new byte[128];
-                    hash_compress_128_str(mid, ans128);
-                    return ans128;
-                case CompressLevel.x64:
-                    var ans64 = new byte[64];
-                    hash_compress_64_str(mid, ans64);
-                    return ans64;
-                case CompressLevel.x32:
-                    var ans32 = new byte[32];
-                    hash_compress_32_str(mid, ans32);
-                    return ans32;
-                case CompressLevel.x16:
-                    var ans16 = new byte[16];
-                    hash_compress_16_str(mid, ans16);
-                    return ans16;
-                case CompressLevel.x8:
-                    var ans8 = new byte[8];
-                    hash_compress_8_str(mid, ans8);
-                    return ans8;
-                case CompressLevel.x4:
-                    var ans4 = new byte[4];
-                    hash_compress_4_str(mid, ans4);
-                    return ans4;
-                default: return Array.Empty<byte>();
-            }
+            case CompressLevel.x128:
+                var ans128 = new byte[128];
+                hash_compress_128_str(mid, ans128);
+                return ans128;
+            case CompressLevel.x64:
+                var ans64 = new byte[64];
+                hash_compress_64_str(mid, ans64);
+                return ans64;
+            case CompressLevel.x32:
+                var ans32 = new byte[32];
+                hash_compress_32_str(mid, ans32);
+                return ans32;
+            case CompressLevel.x16:
+                var ans16 = new byte[16];
+                hash_compress_16_str(mid, ans16);
+                return ans16;
+            case CompressLevel.x8:
+                var ans8 = new byte[8];
+                hash_compress_8_str(mid, ans8);
+                return ans8;
+            case CompressLevel.x4:
+                var ans4 = new byte[4];
+                hash_compress_4_str(mid, ans4);
+                return ans4;
+            default: return Array.Empty<byte>();
         }
-
-        /// <summary>
-        /// 推断哈希采用的压缩级别, 并检查是否合法
-        /// </summary>
-        /// <param name="compressed">哈希压缩值</param>
-        /// <param name="src">原文</param>
-        /// <param name="lnk">是否拥有连字符</param>
-        /// <returns>压缩级别</returns>
-        /// <exception cref="HashException">不合法判断异常, 级别未找到异常</exception>
-        public static CompressLevel HashCompressLevelParse(string compressed,
-            string src, bool lnk = true)
-        {
-            foreach (CompressLevel item in Enum.GetValues(typeof(CompressLevel)))
-                if (FromString2Hex(src, !lnk, item).Equals(compressed))
-                    return item;
-            throw new HashException(HashException.ErrorType.UndefinedCompressLevel);
-        }
-
-        #endregion
-
-        #region 字符串哈希算法
-
-        /// <summary>
-        /// 进行字符串哈希
-        /// </summary>
-        /// <param name="str">字符串</param>
-        /// <param name="clv">压缩级别</param>
-        /// <returns>哈希后的Byte数组</returns>
-        public static byte[] FromString(string str,
-            CompressLevel clv = CompressLevel.x64)
-        {
-            var array = Encoding.UTF8.GetBytes(str);     //  源字符串转 byte[]
-            var mid = new byte[2048];                    //  存储哈希值
-            hash_str(array, mid, array.Length);             //  哈希运算
-            var rst = Compress(ref mid, clv);            //  哈希压缩运算
-            return rst;                                     //  返回哈希压缩值
-        }
-
-        /// <summary>
-        /// 进行字符串哈希(返回十六进制字符串)
-        /// </summary>
-        /// <param name="str">字符串</param>
-        /// <param name="rmLink">是否移除连字符</param>
-        /// <param name="clv">压缩级别</param>
-        /// <returns>十六进制哈希字符串</returns>
-        public static string FromString2Hex(string str, bool rmLink = false,
-            CompressLevel clv = CompressLevel.x64)
-        {
-            var array = Encoding.UTF8.GetBytes(str);     //  源字符串转 byte[]
-            var mid = new byte[2048];                    //  存储哈希值
-            hash_str(array, mid, array.Length);             //  哈希运算
-            var rst = Compress(ref mid, clv);            //  哈希压缩运算
-            var ans = BitConverter.ToString(rst);        //  哈希压缩运算转十六进制字符串
-            return rmLink ? ans.Replace("-", "") : ans;     //  返回字符串, 据参数删除连字符
-        }
-
-        /// <summary>
-        /// 进行字符串哈希
-        /// </summary>
-        /// <param name="str">字符串</param>
-        /// <returns>哈希后的Byte数组</returns>
-        public static byte[] FromString_WithoutCompress(string str)
-        {
-            byte[] array = Encoding.UTF8.GetBytes(str);     //  源字符串转 byte[]
-            byte[] mid = new byte[2048];                    //  存储哈希值
-            hash_str(array, mid, array.Length);             //  哈希运算
-            return mid;                                     //  返回哈希压缩值
-        }
-
-        /// <summary>
-        /// 进行字符串哈希(返回十六进制字符串, 不压缩)
-        /// </summary>
-        /// <param name="str">字符串</param>
-        /// <param name="rmLink">是否移除连字符</param>
-        /// <returns>十六进制不压缩哈希字符串</returns>
-        public static string FromString2Hex_WithoutCompress(string str,
-            bool rmLink = false)
-        {
-            byte[] array = Encoding.UTF8.GetBytes(str);     //  源字符串转 byte[]
-            byte[] mid = new byte[2048];                    //  存储哈希值
-            hash_str(array, mid, array.Length);             //  哈希运算
-            string ans = BitConverter.ToString(mid);        //  哈希压缩运算转十六进制字符串
-            return rmLink ? ans.Replace("-", "") : ans;     //  返回字符串, 据参数删除连字符
-        }
-
-        #endregion
-
-        #region 辅助枚举值定义
-
-        /// <summary>
-        /// 哈希模式
-        /// </summary>
-        /// StringHash  ->  字符串哈希
-        /// FileHash    ->  文件哈希
-        public enum HashMode
-        {
-            StringHash = 0, FileHash = 1
-        }
-
-        /// <summary>
-        /// 哈希压缩级别
-        /// </summary>
-        public enum CompressLevel
-        {
-            // ReSharper disable InconsistentNaming
-            x128 = 5, x64 = 4, x32 = 3, x16 = 2, x8 = 1, x4 = 0
-            // ReSharper restore InconsistentNaming
-        }
-
-        #endregion
     }
+
+    /// <summary>
+    /// 推断哈希采用的压缩级别, 并检查是否合法
+    /// </summary>
+    /// <param name="compressed">哈希压缩值</param>
+    /// <param name="src">原文</param>
+    /// <param name="lnk">是否拥有连字符</param>
+    /// <returns>压缩级别</returns>
+    /// <exception cref="HashException">不合法判断异常, 级别未找到异常</exception>
+    public static CompressLevel HashCompressLevelParse(string compressed,
+        string src, bool lnk = true)
+    {
+        foreach (CompressLevel item in Enum.GetValues(typeof(CompressLevel)))
+            if (FromString2Hex(src, !lnk, item).Equals(compressed))
+                return item;
+        throw new HashException(HashException.ErrorType.UndefinedCompressLevel);
+    }
+
+    #endregion
+
+    #region 字符串哈希算法
+
+    /// <summary>
+    /// 进行字符串哈希
+    /// </summary>
+    /// <param name="str">字符串</param>
+    /// <param name="clv">压缩级别</param>
+    /// <returns>哈希后的Byte数组</returns>
+    public static byte[] FromString(string str,
+        CompressLevel clv = CompressLevel.x64)
+    {
+        var array = Encoding.UTF8.GetBytes(str);     //  源字符串转 byte[]
+        var mid = new byte[2048];                    //  存储哈希值
+        hash_str(array, mid, array.Length);             //  哈希运算
+        var rst = Compress(ref mid, clv);            //  哈希压缩运算
+        return rst;                                     //  返回哈希压缩值
+    }
+
+    /// <summary>
+    /// 进行字符串哈希(返回十六进制字符串)
+    /// </summary>
+    /// <param name="str">字符串</param>
+    /// <param name="rmLink">是否移除连字符</param>
+    /// <param name="clv">压缩级别</param>
+    /// <returns>十六进制哈希字符串</returns>
+    public static string FromString2Hex(string str, bool rmLink = false,
+        CompressLevel clv = CompressLevel.x64)
+    {
+        var array = Encoding.UTF8.GetBytes(str);     //  源字符串转 byte[]
+        var mid = new byte[2048];                    //  存储哈希值
+        hash_str(array, mid, array.Length);             //  哈希运算
+        var rst = Compress(ref mid, clv);            //  哈希压缩运算
+        var ans = BitConverter.ToString(rst);        //  哈希压缩运算转十六进制字符串
+        return rmLink ? ans.Replace("-", "") : ans;     //  返回字符串, 据参数删除连字符
+    }
+
+    /// <summary>
+    /// 进行字符串哈希
+    /// </summary>
+    /// <param name="str">字符串</param>
+    /// <returns>哈希后的Byte数组</returns>
+    public static byte[] FromString_WithoutCompress(string str)
+    {
+        byte[] array = Encoding.UTF8.GetBytes(str);     //  源字符串转 byte[]
+        byte[] mid = new byte[2048];                    //  存储哈希值
+        hash_str(array, mid, array.Length);             //  哈希运算
+        return mid;                                     //  返回哈希压缩值
+    }
+
+    /// <summary>
+    /// 进行字符串哈希(返回十六进制字符串, 不压缩)
+    /// </summary>
+    /// <param name="str">字符串</param>
+    /// <param name="rmLink">是否移除连字符</param>
+    /// <returns>十六进制不压缩哈希字符串</returns>
+    public static string FromString2Hex_WithoutCompress(string str,
+        bool rmLink = false)
+    {
+        byte[] array = Encoding.UTF8.GetBytes(str);     //  源字符串转 byte[]
+        byte[] mid = new byte[2048];                    //  存储哈希值
+        hash_str(array, mid, array.Length);             //  哈希运算
+        string ans = BitConverter.ToString(mid);        //  哈希压缩运算转十六进制字符串
+        return rmLink ? ans.Replace("-", "") : ans;     //  返回字符串, 据参数删除连字符
+    }
+
+    #endregion
+
+    #region 辅助枚举值定义
+
+    /// <summary>
+    /// 哈希模式
+    /// </summary>
+    /// StringHash  ->  字符串哈希
+    /// FileHash    ->  文件哈希
+    public enum HashMode
+    {
+        StringHash = 0, FileHash = 1
+    }
+
+    /// <summary>
+    /// 哈希压缩级别
+    /// </summary>
+    public enum CompressLevel
+    {
+        // ReSharper disable InconsistentNaming
+        x128 = 5, x64 = 4, x32 = 3, x16 = 2, x8 = 1, x4 = 0
+        // ReSharper restore InconsistentNaming
+    }
+
+    #endregion
 }
