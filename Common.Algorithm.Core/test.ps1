7e01c7a759d87d96a22a5f9bf4a0fb11f452014c
Dynesshely <catrol@qq.com>
2023-04-21 14:55:13 +0800

    📝 Chore(Scripts): Quick test for `Common.Algorithm.Core`

---
+++ Common.Algorithm.Core/test.ps1
@@ -0,0 +1,9 @@
+﻿
+$fileName="Common.Algorithm.Core.dll"
+$srcDir="build/Debug/"
+$desDir="build/tests/Debug/"
+
+rm ${desDir}${fileName}
+cp ${srcDIr}${fileName} ${desDir}
+
+& ${desDir}Common.Algorithm.Test.exe
---
+++ Common.Algorithm.Core/test.sh
@@ -0,0 +1,10 @@
+﻿#!/usr/bin/bash
+
+fileName=libCommon.Algorithm.Core.so
+srcDir=build/
+desDir=build/tests/
+
+# rm ${desDir}${fileName}
+# cp ${srcDIr}${fileName} ${desDir}
+
+./${desDir}/Common.Algorithm.Test
