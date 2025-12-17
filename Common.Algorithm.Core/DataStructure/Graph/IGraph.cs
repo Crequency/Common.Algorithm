d813a56a1788665d59669b490e843f436790bb12
Dynesshely <dynesshely@gmail.com>
2025-12-17 19:12:00 +0800

    💾 Feat: Add IGraph interface and GraphTypes enum

---
+++ Common.Algorithm.Core/DataStructure/Graph/IGraph.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common.Algorithm.Core.DataStructure.Graph;
+
+internal interface IGraph
+{
+    bool IsTotallyConnected { get; set; }
+
+    GraphTypes Type { get; set; }
+}
---
+++ Common.Algorithm.Core/DataStructure/Graph/Properties/GraphTypes.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public enum GraphTypes
+{
+    Directed = 1,
+    Undirected = 2,
+    Mixed = 4,
+}
